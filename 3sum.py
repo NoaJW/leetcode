17b301bdbb6d090a293966e6fce5694e8b9b742f
Noa <jnjw55@gmail.com>
2023-08-06 23:54:32 +0800

    add optimal sol

--- 3sum.py
+++ 3sum.py
@@ -24,8 +24,42 @@ class MySolution:
         return sol
 
 
+# Sort and binary search
+class Solution:
+    def threeSum(self, nums):
+        nums.sort()
+        sol = []
+        
+        for left in range(len(nums) - 2):   # Stop when reached the last 2 numbers 
+            # To avoid duplications from the left side
+            if left > 0 and nums[left] == nums[left - 1]:
+                continue
+            # Taking 2 pointers and using Binary Search
+            mid, right = left + 1, len(nums) - 1
+            while mid < right:
+                curr_sum = nums[left] + nums[mid] + nums[right]
+                # Increase the value from the left side
+                if curr_sum < 0:
+                    mid += 1
+                # Decrease the value from the right side
+                elif curr_sum > 0:
+                    right -= 1
+                else:
+                    sol.append([nums[left], nums[mid], nums[right]])
+                    # Avoid repetition from the sub-array's left side
+                    while mid < right and nums[mid] == nums[mid + 1]:
+                        mid += 1
+                    # Avoid repetition from the sub-array's right side
+                    while mid < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    
+                    mid += 1
+                    right -= 1
+        return sol
+    
+
 # Driver
-nums = [0,1,1]
+nums = [-1,0,1,2,-1,-4]
 
 s = MySolution()
 print(s.threeSum(nums))
--- test.py
+++ test.py
@@ -1,14 +1,4 @@
-# sol = [[-1, 0, 1], [-1, 2, -1], [0, 1, -1]]
-sol = []
+sol = [[-1, 0, 1], [-1, 2, -1], [0, 1, -1]]
 
-
-sol.append(sorted([-1, 0, 1]))
-sol.append(sorted([-1, 2, -1]))
-
-if sorted([0, 1, -1]) not in sol: 
-    print("in there")
-else:
-    print("not")
-# sol.append(sorted([0, 1, -1]))
-
-print(sol)
+for i in range(10):
+    print(i)
